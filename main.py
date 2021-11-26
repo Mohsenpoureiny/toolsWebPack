e81465e5943653f1f424aa8db515f4f9b98d5898
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2021-11-26 10:03:59 +0330

    add delete

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ import uuid
 app = Flask(__name__)
 
 
+def deleteZipFiles():
+    directory = "./"
+    files_in_directory = os.listdir(directory)
+    filtered_files = [
+        file for file in files_in_directory if file.endswith(".zip")]
+    for file in filtered_files:
+        path_to_file = os.path.join(directory, file)
+        os.remove(path_to_file)
+
+
 @app.route("/")
 def home():
     dirlist = os.listdir("./files")
@@ -36,11 +46,30 @@ def download(category, name):
 
 @app.route("/download/<category>")
 def downloadCategory(category):
+    deleteZipFiles()
     arcName = category+"__"+str(uuid.uuid4())
     shutil.make_archive(arcName, 'zip', './files/'+category)
     return send_from_directory("./", arcName+".zip")
 
 
+@app.route("/delete/<category>/<name>")
+def deleteFile(category, name):
+    os.remove("./files/" + unquote_plus(category)+"/"+unquote_plus(name))
+    return redirect(url_for('home'))
+
+
+@app.route("/delete/<category>")
+def deleteCategory(category):
+    os.rmdir("./files/" + unquote_plus(category))
+    return redirect(url_for('home'))
+
+
+@app.route("/create/<category>")
+def createCategory(category):
+    os.mkdir("./files/" + unquote_plus(category))
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
-    app.run(debug=True, host="0.0.0.0",port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
